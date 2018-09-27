59777638980c815fdb47cd00e84cd376fbc19216
PennController <39499242+PennController@users.noreply.github.com>
2018-09-27 12:55:53 -0400

    Update example_data.js

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,1 +1,10 @@
+PennController.ResetPrefix(null);
 
+PennController(
+    newText("green", "How warm is the color green to you?")
+        .print()
+    ,
+    newScale("judgment", 5) // 5-point scale
+        .print()
+        .wait()             // Validate upon choice
+);
