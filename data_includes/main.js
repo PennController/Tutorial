793118b1e83da2bbfb6b1f46d7a2b1e2b03723d2
PennController <39499242+PennController@users.noreply.github.com>
2019-04-09 19:58:41 -0400

    No URL

--- data_includes/main.js
+++ data_includes/main.js
@@ -4,10 +4,10 @@ PennController(
     newText("test sentence", "The fish swim in a tank which is perfectly round")
         .print()        // A test sentence
     ,
-    newImage("competitor", "http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/1fishSquareTank.png")
+    newImage("competitor", "1fishSquareTank.png")
         .print()        // An image with 1 fish that swims in a square tank
     ,
-    newImage("target", "http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/2fishRoundTank.png")
+    newImage("target", "2fishRoundTank.png")
         .print()        // An image with 2 fish that swim in a round tank
     ,
     newSelector("tank") // We indicate that target+competitor belong to a selection group
